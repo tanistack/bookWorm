74b0e0d7693eca992f69cb5a5f06a9ba99785556
tanistack <tanistack@gmail.com>
2026-02-24 11:02:26 +0300

    Render Cron Job Added

--- src/app.js
+++ src/app.js
@@ -21,17 +21,19 @@ const port = process.env.PORT || 5000;
 
 //dbconnection
 const dbConnection = require("./db/connect");
+import cron from "./lib/cron";
 
+cron.start();
 
 const app = express();
 app.use(express.json());
 
-app.set('trust proxy', 1);
+app.set("trust proxy", 1);
 app.use(
   rateLimiter({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100, // limit each IP to 100 requests per windowMs
-  })
+  }),
 );
 
 app.use(helmet());
@@ -39,29 +41,26 @@ app.use(cors());
 app.use(xss());
 
 //routes
-app.get("/", (req,res)=>{
-    res.send("The Book Worm Leads");
+app.get("/", (req, res) => {
+  res.send("The Book Worm Leads");
 });
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/books", authenticateUser, booksRouter);
 
-
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
+const start = async () => {
+  await dbConnection();
 
-const start = async ()=>{
-    await dbConnection();
-
-    try {
-        app.listen(port, ()=>{
-        console.log(`Server up and running and listening on ${port}`)
+  try {
+    app.listen(port, () => {
+      console.log(`Server up and running and listening on ${port}`);
     });
-
-    } catch (error) {
-       console.log(`Server error, ${error}`); 
-    }
-}
+  } catch (error) {
+    console.log(`Server error, ${error}`);
+  }
+};
 
 start();
