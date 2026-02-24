987c3e38139d232a842e7db5ebd29988de21061f
tanistack <tanistack@gmail.com>
2026-02-24 18:40:03 +0300

    Update Book model

--- src/models/book.js
+++ src/models/book.js
@@ -1,37 +1,36 @@
 const mongoose = require("mongoose");
 
-
-const BookSchema = mongoose.Schema({
-    name:{
-        type:String,
-        required:[true, "The book name is required"],
-        unique:true,
-        trim:true
+const BookSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "The book name is required"],
+      unique: true,
+      trim: true,
     },
 
-    caption:{
-        type:String,
-        required:[true, "The book caption is required"]
+    caption: {
+      type: String,
+      required: [true, "The book caption is required"],
     },
-    rating:{
-        type:String,
-        min: 1,
-        max: 5,
-    },
-
-    image:{
-        type:String,
+    rating: {
+      type: String,
+      min: 1,
+      max: 5,
     },
 
-    createdBy:{
-        type: mongoose.Types.ObjectId,
-        ref:"User",
-        required: [true, "Please login or sign up to continue"]
-    }
-}, 
+    image: {
+      type: String,
+    },
 
-{timestamps:true}
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "Please login or sign up to continue"],
+    },
+  },
 
+  { timestamps: true },
 );
 
 module.exports = mongoose.model("Book", BookSchema);
