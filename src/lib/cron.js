6b35d9989007ad760f3b2c858f5e6d8bcaabd9e3
tanistack <tanistack@gmail.com>
2026-02-24 11:48:01 +0300

    Fixed Cron

--- src/lib/cron.js
+++ src/lib/cron.js
@@ -1,11 +1,14 @@
-const { cron } = require("cron");
-const { https } = require("https");
+const { CronJob } = require("cron");
+const https = require("https");
 
-const job = new cron.CronJob("*/14 * * * *", function () {
+const job = new CronJob("*/14 * * * *", function () {
   https
     .get(process.env.API_URL, (res) => {
-      if (res.statusCode === 200) console.log("GET request sent successfully");
-      else console.log("GET request failed", res.statusCode);
+      if (res.statusCode === 200) {
+        console.log("GET request sent successfully");
+      } else {
+        console.log("GET request failed", res.statusCode);
+      }
     })
     .on("error", (e) => console.error("Error while sending request", e));
 });
