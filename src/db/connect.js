5f9004fc9f742b28f2a21122c4002020db1808b0
tanistack <tanistack@gmail.com>
2026-02-24 08:51:52 +0300

    Cleaned out unsused code

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,32 +1,17 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
-
   try {
-    const conn = mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`)
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error('Database connection failed:')
-    console.error(error.message)
+    console.error('Database connection failed:');
+    console.error(error.message);
 
     // Exit app if DB fails
-    process.exit(1)
+    process.exit(1);
   }
-}
-
-
-
-
-
-
-
-
-
-
-
+};
 
 module.exports = connectDB;
