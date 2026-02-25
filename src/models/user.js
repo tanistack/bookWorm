1cabbd1b66ac73b8f04d5b8185044e90f5017494
tanistack <tanistack@gmail.com>
2026-02-25 11:08:37 +0300

    Added profileImage to User

--- src/models/user.js
+++ src/models/user.js
@@ -2,40 +2,47 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-
-const UserSchema = mongoose.Schema({
-    username:{
-        type: String,
-        required:[true, "Please provide a username"],
-        maxlength:[20, "The username should not be more than 20 characters long"],
-        unique:true
+const UserSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Please provide a username"],
+      maxlength: [
+        20,
+        "The username should not be more than 20 characters long",
+      ],
+      unique: true,
     },
 
-    email:{
-        type: String,
-        required:[true, "Please provide an email address"],
-         match: [
+    email: {
+      type: String,
+      required: [true, "Please provide an email address"],
+      match: [
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-        'Please provide a valid email',
-        ],
-        unique: true,
+        "Please provide a valid email",
+      ],
+      unique: true,
     },
-    password:{
-        type: String,
-        required:[true, "The password is required"],
-        minlength:[5, "The password should be more than 5 characters long"]
-    }
-},
 
-{timestamps: true}
+    profileImage: {
+      type: String,
+      default: "",
+    },
 
+    password: {
+      type: String,
+      required: [true, "The password is required"],
+      minlength: [5, "The password should be more than 5 characters long"],
+    },
+  },
+
+  { timestamps: true },
 );
 
-
-UserSchema.pre('save', async function () {
-  const salt = await bcrypt.genSalt(10)
-  this.password = await bcrypt.hash(this.password, salt)
-})
+UserSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 UserSchema.methods.createJWT = function () {
   return jwt.sign(
@@ -43,15 +50,13 @@ UserSchema.methods.createJWT = function () {
     process.env.JWT_SECRET,
     {
       expiresIn: process.env.JWT_LIFETIME,
-    }
-  )
-}
+    },
+  );
+};
 
 UserSchema.methods.comparePassword = async function (candidatePassword) {
-  const isMatch = await bcrypt.compare(candidatePassword, this.password)
-  return isMatch
-}
-
-
+  const isMatch = await bcrypt.compare(candidatePassword, this.password);
+  return isMatch;
+};
 
 module.exports = mongoose.model("User", UserSchema);
