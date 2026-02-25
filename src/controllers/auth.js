ff8019388d195238d06a8d45b8e0f6b38265be8a
tanistack <tanistack@gmail.com>
2026-02-25 11:40:59 +0300

    Updated Auth Controller

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,45 +1,53 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const {StatusCodes} = require("http-status-codes");
-const {BadRequestError, UnauthenticatedError} = require("../errors");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, UnauthenticatedError } = require("../errors");
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 
-
-const register = async (req, res, next) => {
+const register = async (req, res) => {
   const user = await User.create({ ...req.body });
 
-  const token = user.createJWT(); // ← CALL the function
+  const token = user.createJWT();
 
   res.status(StatusCodes.CREATED).json({
-    user: { username: user.username },
-    token: token
+    user: {
+      username: user.username,
+      userId: user._id,
+      profileImage: user.profileImage,
+    },
+    token,
   });
 };
 
 const login = async (req, res) => {
-  const { email, password } = req.body
+  const { email, password } = req.body;
 
   if (!email || !password) {
-    throw new BadRequestError('Please provide email and password')
-  }
-  const user = await User.findOne({ email })
-  if (!user) {
-    throw new UnauthenticatedError('Invalid Credentials')
+    throw new BadRequestError("Please provide email and password");
   }
 
-  //compare passwords
-  const isPasswordCorrect = await user.comparePassword(password)
-  if (!isPasswordCorrect) {
-    throw new UnauthenticatedError('Invalid Credentials')
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new UnauthenticatedError("Invalid Credentials");
   }
-  // create login token
-  const token = user.createJWT()
-  res.status(StatusCodes.OK).json({ user: { username: user.username }, token })
-}
+
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError("Invalid Credentials");
+  }
+
+  const token = user.createJWT();
+
+  res.status(StatusCodes.OK).json({
+    user: {
+      username: user.username,
+      userId: user._id,
+      profileImage: user.profileImage,
+    },
+    token,
+  });
+};
 
 module.exports = {
-    register,
-    login
-}
-
+  register,
+  login,
+};
