d05c6f406986c5992ba166d365a16da25d9804ce
tanistack <tanistack@gmail.com>
2026-02-25 10:36:11 +0300

    Added user end book

--- src/routes/books.js
+++ src/routes/books.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const router = express.Router();
-const {getAllBooks,createBook,getBook,updateBook,deleteBook} = require("../controllers/books");
+const Book = require("../routes/books");
+const {
+  getAllBooks,
+  createBook,
+  getBook,
+  updateBook,
+  deleteBook,
+} = require("../controllers/books");
 
 router.route("/").get(getAllBooks).post(createBook);
 router.route("/:id").get(getBook).patch(updateBook).delete(deleteBook);
+
+router.get("/user", protect, async (req, res) => {
+  try {
+    // ✅ req.user comes from JWT
+    const books = await Book.find({ user: req.user }).sort({ createdAt: -1 });
+
+    res.json(books);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
 
 module.exports = router;
