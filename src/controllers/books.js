588800973e1180f5d2dab02eca3179e7b97e4a3c
tanistack <tanistack@gmail.com>
2026-02-25 20:06:53 +0300

    Remove Skip

--- src/controllers/books.js
+++ src/controllers/books.js
@@ -5,9 +5,9 @@ const { BadRequestError, NotFoundError } = require("../errors");
 const getAllBooks = async (req, res) => {
   const books = await Book.find({})
     .sort("-createdAt")
-    .populate("createdBy", "username profileImage")
-    .skip(skip)
-    .limit(limit);
+    .populate("createdBy", "username profileImage");
+  // .skip(skip)
+  // .limit(limit);
 
   const count = books.length;
   res.status(StatusCodes.OK).json({ books, count: count });
