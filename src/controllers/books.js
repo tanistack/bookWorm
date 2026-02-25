c427c3743b6f715149a74a47a96a753ec3b2c270
tanistack <tanistack@gmail.com>
2026-02-25 19:42:58 +0300

    Get All Books Fix

--- src/controllers/books.js
+++ src/controllers/books.js
@@ -3,10 +3,10 @@ const { StatusCodes } = require("http-status-codes");
 const { BadRequestError, NotFoundError } = require("../errors");
 
 const getAllBooks = async (req, res) => {
-  const books = await Book.find({ createdBy: req.user.userId })
+  const books = await Book.find({})
     .sort("-createdAt")
-    .populate("createdBy", "username profileImage")
-    .sort("-createdAt");
+    .populate("createdBy", "username profileImage");
+
   const count = books.length;
   res.status(StatusCodes.OK).json({ books, count: count });
 };
